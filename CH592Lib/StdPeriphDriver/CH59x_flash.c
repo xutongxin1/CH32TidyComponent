de089694d7f617a9c10829533ef733b6b075f78e
xtx <2656823393@qq.com>
2024-10-04 22:56:14 +0800

    更新部分库

--- CH592Lib/StdPeriphDriver/CH59x_flash.c
+++ CH592Lib/StdPeriphDriver/CH59x_flash.c
@@ -122,7 +122,7 @@ uint8_t UserOptionByteClose_SWD(void)
 
     FLASH_ROM_READ(0x14, &s, 4);
 
-    if(s == 0xF3F9BDA9)
+    if(s == 0xF5F9BDA9)
     {
         FLASH_EEPROM_CMD(CMD_GET_ROM_INFO, 0x7EFFC, &s, 4);
 
@@ -177,9 +177,7 @@ void UserOptionByte_Active(void)
  */
 void GET_UNIQUE_ID(uint8_t *Buffer)
 {
-    uint16_t temp;
     FLASH_EEPROM_CMD( CMD_GET_ROM_INFO, ROM_CFG_MAC_ADDR, Buffer, 0 );
-    temp = (Buffer[0]|(Buffer[1]<<8)) + (Buffer[2]|(Buffer[3]<<8)) + (Buffer[4]|(Buffer[5]<<8));
-    Buffer[6] = temp&0xFF;
-    Buffer[7] = (temp>>8)&0xFF;
+    Buffer[6] = 0;
+    Buffer[7] = 0;
 }
