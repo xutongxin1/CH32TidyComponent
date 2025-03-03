306ad36324dc42aee4c3c55c879b980801d8ecdb
xtx <2656823393@qq.com>
2025-03-03 17:58:19 +0800

    feat：WS2812驱动移植，待测试

---
+++ CH592_Node/APP/WS2812.c
@@ -0,0 +1,78 @@
+//
+// Created by 86180 on 2024/7/21.
+//
+
+#include "WS2812.h"
+/* PWM Output Mode Definition */
+uint16_t color_buf[COLOR_BUFFER_LEN] = {0};
+__attribute__((aligned(4))) uint32_t PwmBuf[100];
+void WS2812Init(){
+
+    GPIOA_ModeCfg(GPIO_Pin_11, GPIO_ModeOut_PP_5mA);
+    // GPIOPinRemap(ENABLE, RB_PIN_TMR2);//映射到PB11
+
+    PRINT("TMR2 DMA PWM\n");
+    TMR2_PWMCycleCfg(75); // 周期 2000us  主频是60Mhz 每秒震荡60M次 震荡60次为1微秒
+    TMR2_PWMInit(Low_Level, PWM_Times_1);
+    TMR2_DMACfg(ENABLE, (uint32_t)&color_buf[0], (uint32_t)&color_buf[COLOR_BUFFER_LEN], Mode_Single);
+    TMR2_PWMEnable();
+    TMR2_Enable();
+
+    /* 开启计数溢出中断，计满100个周期进入中断 */
+    // TMR2_ClearITFlag(TMR1_2_IT_DMA_END);
+    // TMR2_ITCfg(ENABLE, TMR1_2_IT_DMA_END);
+    // PFIC_EnableIRQ(TMR2_IRQn);
+
+    for (int i = 0; i < LED_NUM; i++) {
+        setPixelColor(i, 0, 0, 255);
+    }
+    w2812_sync();
+
+}
+
+
+
+///
+/// \param id
+/// \param r
+/// \param g
+/// \param b
+void setPixelColor(uint16_t id, uint8_t r, uint8_t g, uint8_t b)
+{
+    int i = 0, j = id * 24u;
+    if (id >= 4) {
+        return;
+    }
+
+    for (i = 0; i < 8; i++) {
+        if (g & (0x80 >> i)) {
+            color_buf[j] = CODE_1;
+        } else {
+            color_buf[j] = CODE_0;
+        }
+        j++;
+    }
+    for (i = 0; i < 8; i++) {
+        if (r & (0x80 >> i)) {
+            color_buf[j] = CODE_1;
+        } else {
+            color_buf[j] = CODE_0;
+        }
+        j++;
+    }
+    for (i = 0; i < 8; i++) {
+        if (b & (0x80 >> i)) {
+            color_buf[j] = CODE_1;
+        } else {
+            color_buf[j] = CODE_0;
+        }
+        j++;
+    }
+}
+
+void w2812_sync()
+{
+    while(R32_TMR2_DMA_NOW != R32_TMR2_DMA_END) {}
+    TMR2_DMACfg(ENABLE, (uint32_t)&color_buf[0], (uint32_t)&color_buf[COLOR_BUFFER_LEN], Mode_Single);
+    TMR2_PWMEnable();
+}
---
+++ CH592_Node/APP/include/WS2812.h
@@ -0,0 +1,27 @@
+//
+// Created by xtx on 25-3-3.
+//
+
+#ifndef WS2812_H
+#define WS2812_H
+
+#include "CH59x_common.h"
+#include <MESH_LIB.h>
+
+//¸ßµÍÕ¼±È
+#define CODE_1  7
+#define CODE_0 3
+
+
+#define RESET_LEN    (60)
+#define  LED_NUM 8
+#define COLOR_BUFFER_LEN ((LED_NUM*(3*8))+RESET_LEN)
+#define MAX_STEP (200)
+#define LIST_SIZE(list) (sizeof(list)/sizeof(list[0]))
+#define hex2rgb(c) (((c)>>16)&0xff),(((c)>>8)&0xff),((c)&0xff)
+
+void setPixelColor(uint16_t id, uint8_t r, uint8_t g, uint8_t b);
+void w2812_sync();
+void WS2812Init();
+
+#endif //WS2812_H
